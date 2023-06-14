8f28ed09f5a9eb0ad6d2d23e6e8bcbe91962f19c
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-14 15:23:03 +0300

    state içerik

---
+++ StateKonu/StateKonusu/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { useState } from 'react'
+
+
+function App() {
+const [value, setValue] = useState(0);
+
+const handleClick = () => {
+ setValue(value+1);
+};
+
+  return (
+    <>
+    <div className='app'>
+      <button 
+      onClick={handleClick}
+        >
+          Kurs Ekle
+          </button>
+      <div>Kurs Sayısı: {value}</div>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
